afc0a6262a25138553a75250b6e8b93d8f1fee46
Wang Yulin <aaron.wangyulin@gmail.com>
2021-03-27 17:07:51 +0800

    bug fixed

--- controller/register.js
+++ controller/register.js
@@ -26,7 +26,7 @@ const handleRegister = (req, res, db, bcrypt) => {
             .then(trx.commit)
             .catch(trx.rollback)
         })
-        .catch(err => console.log(err))
+        .catch(err => res.status(400).json('unable to register'))
     }
 
 export default handleRegister;
