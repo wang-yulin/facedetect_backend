5257beba75a17037202801360d0ac81ddddfb34f
Wang Yulin <aaron.wangyulin@gmail.com>
2021-03-27 15:28:11 +0800

    connection database

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.use(cors());
 
 const db = knex({
     client: 'pg',
-    connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true
-    }
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+        }
   });
 
 
